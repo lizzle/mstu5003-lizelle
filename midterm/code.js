ceacd8a107919680ac03f4c789b5a6e6246d1b9c
lizzle <lizelle@Lizelles-MacBook-Air.local>
2016-11-16 16:46:59 -0500

    adding content
    MSTU5003 home page and midterm materials

---
+++ midterm/code.js
@@ -0,0 +1,25 @@
+// Load Firebase for the save/submit functions below
+
+// THINK ABOUT IT
+/*
+When you select an option for the radio button, a response will appear below depending on the one you chose.
+if (the user selects option A) {
+  display this text;
+} else {
+  display this information text;
+}
+
+The "read about it" and "write about it" sections will also appear after a choice is made.
+*/
+
+
+// WRITE ABOUT IT
+/*
+Create variables to reference both the save and submit buttons and assign them to the IDs from HTML
+
+Add event listeners for each button that will do the following:
+  If you click 'save', this information will be backed up to Firebase.
+
+  If you click 'submit', this will prompt you to create an account to save and access this writing.
+  It will then lead to the next set of questions with the same progression.
+*/
